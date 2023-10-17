926b01b467884ea374b0ede59552ec4366f606af
tusharshaha <tusharshaha128@gmail.com>
2023-10-17 13:25:24 +0600

    cleaned up app component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools"
 import { Toaster } from "react-hot-toast";
@@ -11,12 +10,10 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <Router>
-      <React.StrictMode>
-        <App />
-        <Toaster />
-      </React.StrictMode>
-    </Router>
-    <ReactQueryDevtools initialIsOpen={false} position="bottom-right"/>
+    <React.StrictMode>
+      <App />
+      <Toaster />
+    </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
   </QueryClientProvider>
 );
