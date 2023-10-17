3e022000525177558e6ad0630780a2f91f66efa7
Arshad <arshad.syed.anwar@gmail.com>
2023-10-17 03:34:38 +0100

    added user list and register function

---
+++ client/src/pages/Users/Users.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import axiosRequest from "../../utils/axios.service";
+import { useEffect } from "react";
+import { toast } from 'react-hot-toast';
+import Table from "./Table";
+import { useQuery } from 'react-query';
+
+const Users = () => {
+	const fetchUsers = async () => {
+		return axiosRequest.get('/user');
+	};
+	const { isLoading, error, isError, data } = useQuery('users', fetchUsers);
+	useEffect(() => {
+    if (isError) {
+      toast.error(error, { id: "user" });
+    }
+  }, [isError])
+
+	console.log(data);
+	return (
+		<div>
+			<Table data={data?.users}/>
+		</div>
+	);
+};
+
+export default Users;
