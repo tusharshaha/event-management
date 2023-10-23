d851dfe6840f6de276b90a9a4f9453b5227f6503
Imrancu <mdimran3772@gmail.com>
2023-10-23 21:48:26 +0600

    Project page design is completed

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -1,58 +1,15 @@
-import React from 'react';
-import ProjectBanner from './components/ProjectBanner';
+import React from "react";
+import ProjectBanner from "./components/ProjectBanner";
+import ImageGallery from "./components/ImageGallery";
+import RecentProject from "./components/RecentProject";
 
 const ProjectsPage = () => {
-  // Sample project data
-  const projects = [
-    {
-      id: 1,
-      title: 'Corporate Gala Dinner',
-      location: 'New York, NY',
-      date: 'April 12, 2023',
-      description:
-        'A luxurious gala dinner event held at a prestigious venue, featuring celebrity guests, fine dining, and live entertainment.',
-      imageSrc: 'https://via.placeholder.com/800x600',
-    },
-    {
-      id: 2,
-      title: 'Community Music Festival',
-      location: 'Los Angeles, CA',
-      date: 'July 22, 2023',
-      description:
-        'An annual music festival that brings together local artists and music enthusiasts for a day of live performances and fun.',
-      imageSrc: 'https://via.placeholder.com/800x600',
-    },
-    // Add more project entries here
-  ];
-
   return (
-    <div>
-      <ProjectBanner/>
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-3xl font-semibold text-center mb-8">
-          Our Recent Projects
-        </h1>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {projects.map((project) => (
-            <div
-              key={project.id}
-              className="bg-white rounded-lg shadow-lg overflow-hidden"
-            >
-              <img
-                className="w-full h-56 object-cover"
-                src={project.imageSrc}
-                alt={project.title}
-              />
-              <div className="p-4">
-                <h2 className="text-xl font-semibold">{project.title}</h2>
-                <p className="text-gray-600">{project.location}</p>
-                <p className="text-gray-600">{project.date}</p>
-                <p className="mt-4 text-gray-700">{project.description}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+    <div className="bg-gradient-to-r from-gray-300 from-10% via-gray-200 via-30% to-gray-100 to-90% ">
+      <ProjectBanner />
+      <div className="container mx-auto py-12 px-4">
+        <ImageGallery />
+        <RecentProject />
       </div>
     </div>
   );
--- client/src/pages/Projects/components/ProjectBanner.jsx
+++ client/src/pages/Projects/components/ProjectBanner.jsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import about from '../../../assets/banner/banner3.png'
+import React from "react";
+import banner from "../../../assets/banner/projectBanner.jpg";
 
 const ProjectBanner = () => {
   return (
-    <div className="">
-        <div className="relative">
-        <img src="https://cdn.pixabay.com/photo/2022/02/15/06/53/band-7014366_1280.jpg" alt="Your Image" className="w-full rounded-b-full" />
-        <div className="absolute rounded-b-full inset-0 bg-gradient-to-b from-gray-500 to-black opacity-30"></div>
-
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="w-2/4">
-            <h2 className="text-white text-center lg:text-5xl text-4xl font-semibold">
-            Elevate Your Events with Us
-            </h2>
-            <p className="text-xl mt-4 md:text-xl text-center text-white">
-            Creating unforgettable experiences for every occasion.
-          </p>
-          </div>
-        </div>
-      </div>
-      
+    <div className="mt-20">
+      <img src={banner} alt="Your Image" className="w-full h-auto" />
     </div>
   );
 };
